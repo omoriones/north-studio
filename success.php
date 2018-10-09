3f8ea6be454ddd6b0fc0b6e0c71c38169b4bba23
Olivier <omoriones@gmail.com>
2018-10-09 16:38:48 +0200

    Update success.php

--- success.php
+++ success.php
@@ -2,25 +2,37 @@
 <html>
 <head>
 
+  <meta charset="UTF-8">
+
+    <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+  <!-- Librerias internas -->
+	<link rel="stylesheet" type="text/css" href="css/css.css" />
+	<script type="text/javascript" src="js/common.js"></script>
+
 </head>
 <body>
 
   <!-- Barra de navegacion -->
-  <?php include("nav.php"); ?>
+  <div id="nav-bar"></div>
+  <script>
+    $(function(){
+      $("#nav-bar").load("nav.html");
+    });
+  </script>
 
   <section>
-    <form name="success" action="form.php" method="post">
 
     <h6>¡Hemos recibido tu solicitud de valoración correctamente!</h6> <br>
     <p>En 24h recibirás un email con una estimación del valor de tu vivienda.</p> <br>
 
-    <button type="submit" value="volver" onclick="">Volver a la página principal</button>
+    <a href="form.html">Volver a la página principal</button>
 
     <hr>
 
     <p>Si tienes cualquier pregunta, no dudes en llamarnos a +34 902 734 050.</p>
 
-    </form>
     </section>
 
   </body>
