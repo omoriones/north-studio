a8d2bf1a6466f7c44a1974892cc903e3f45ae64a
Olivier <omoriones@gmail.com>
2018-10-12 11:31:43 +0200

    Update validate-form.js

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,28 +1,3 @@
-$(document).ready(function() {
-
-	//Show/hide hamburger menu
-	mostrarOcultarHamburger();
-
-});
-
-$(window).resize(function() {
-
-	//Show/hide hamburger menu
-	mostrarOcultarHamburger();
-
-});
-
-
-//Show/hide hamburger menu
-function mostrarOcultarHamburger() {
-	if (window.matchMedia('screen and (max-width: 1024px)').matches) {
-		$(".hamburger").show();
-		$("#nav-bar-options").hide();
-	} else {
-		$(".hamburger").hide();
-		$("#nav-bar-options").show();
-	}
-}
 
 //Validate form
 function validateForm() {
